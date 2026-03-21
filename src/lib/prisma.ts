c31a7b73dd7578e5717f88c4b2a97ce7a8b1035c
waroon <waroon01@gmail.com>
2026-03-21 21:41:45 +0700

    change pnpm first

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,23 +1,16 @@
-import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
+import { PrismaClient } from '../app/generated/prisma/client'
+
+const connectionString = `${process.env.DATABASE_URL}`
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+    prisma: PrismaClient | undefined
 }
 
-const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL
+const adapter = new PrismaPg({ connectionString })
 
-  if (!connectionString) {
-    throw new Error('DATABASE_URL environment variable is not set')
-  }
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma
 }
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
